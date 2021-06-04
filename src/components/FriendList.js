db5268d50f8ce5ad2196d0b317769f9345c7595b
AntonLovkin <anton28111987@gmail.com>
2021-06-04 21:18:25 +0300

    update Friends.js

--- src/components/FriendList.js
+++ src/components/FriendList.js
@@ -1,27 +1,33 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import FriendListItem from './FriendListItem';
-import friendlist from '../styles/FriendList.module.css'
+import friendlist from '../styles/FriendList.module.css';
 
 const FriendList = ({ friends }) => (
-    
-    <ul className={friendlist.friend_list}>
-        {/* <p className={friendlist.title}>Friend List</p> */}
-        {friends.map(friend => (
-            <FriendListItem
-                avatar={friend.avatar}
-                name={friend.name}
-                isOnline={friend.isOnline}
-                id={friend.id} />
+  <ul className={friendlist.friend_list}>
+    {/* <p className={friendlist.title}>Friend List</p> */}
+    {friends.map(friend => (
+      <FriendListItem
+        avatar={friend.avatar}
+        name={friend.name}
+        isOnline={friend.isOnline}
+        id={friend.id}
+      />
     ))}
-    </ul>)
-    
-FriendList.defaultProps = {
-    
-};
- 
-FriendList.propTypes = {
-  friends: PropTypes.array.isRequired,
-}
+  </ul>
+);
 
-export default FriendList      
+FriendList.defaultProps = {};
+
+FriendList.propTypes = {
+  friends: PropTypes.arrayOf(
+    PropTypes.shape({
+      name: PropTypes.string,
+      avatar: PropTypes.string.isRequired,
+      isOnline: PropTypes.bool.isRequired,
+      id: PropTypes.number.isRequired,
+    }),
+  ).isRequired,
+};
+
+export default FriendList;
--- src/components/FriendListItem.js
+++ src/components/FriendListItem.js
@@ -1,24 +1,24 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import friendlist from '../styles/FriendList.module.css'
+import friendlist from '../styles/FriendList.module.css';
 
-const FriendListItem = ({avatar, name, isOnline, id}) => (
+const FriendListItem = ({ avatar, name, isOnline, id }) => (
   <li className={isOnline ? friendlist.item : friendlist.disabled} key={id}>
-    <span class="status">{isOnline}</span>
-   <img class="avatar" src={avatar} alt={name} width="48" />
-   <p class="name">{name}</p>
- </li>
-)
+    <span className="status">{isOnline}</span>
+    <img className="avatar" src={avatar} alt={name} width="48" />
+    <p className="name">{name}</p>
+  </li>
+);
 
 FriendListItem.defaultProps = {
-    name: "Friend Name",
+  name: 'Friend Name',
 };
- 
+
 FriendListItem.propTypes = {
   name: PropTypes.string,
   avatar: PropTypes.string.isRequired,
   isOnline: PropTypes.bool.isRequired,
-  id: PropTypes.number.isRequired, 
-}
+  id: PropTypes.number.isRequired,
+};
 
-export default FriendListItem
+export default FriendListItem;
