d2d8a60469d29e67cef1d3b1afd0e386b2117abb
AntonLovkin <anton28111987@gmail.com>
2021-06-04 21:18:07 +0300

    update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Profile from "./components/Profile";
+import Profile from './components/Profile';
 import user from './data/user.json';
 import Statistics from './components/Statistics';
 import statisticalData from './data/statistical-data.json';
@@ -8,30 +8,24 @@ import FriendList from './components/FriendList';
 import TransactionHistory from './components/TransactionHistory';
 import transactions from './data/bank-story.json';
 
-const App = () => 
-(<div>
-    {user.map((user) => (
-        <Profile
-           name={user.name}
-           tag={user.tag}
-           location={user.location}
-           avatar={user.avatar}
-                followers={user.stats.followers}
-                views={user.stats.views}
-                likes={user.stats.likes}
-                id={user.tag}
-            />
-       ))}
-        <Statistics
-        title="Upload stats"
-        stats={statisticalData} />;
-    
-    <FriendList
-        friends={friends} />
-    
-    <TransactionHistory
-        items={transactions} />
-        
-     </div>)
+const App = () => (
+  <div>
+    {user.map(user => (
+      <Profile
+        name={user.name}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        followers={user.stats.followers}
+        views={user.stats.views}
+        likes={user.stats.likes}
+        id={user.tag}
+      />
+    ))}
+    <Statistics title="" stats={statisticalData} />;
+    <FriendList friends={friends} />
+    <TransactionHistory items={transactions} />
+  </div>
+);
 
 export default App;
